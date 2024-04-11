5e73b159531be9a53d72a66dc11cee3f6d9291c6
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-11 18:06:55 +0200

    Cart Counter Animation
    - Created bounce animation for cart counter to bounce once qty changes
    - created a side effect in Cart.tsx to apply the bounce

--- ecommerce-kimz/src/components/eCommerce/Cart/Cart.tsx
+++ ecommerce-kimz/src/components/eCommerce/Cart/Cart.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+
 import CartIcon from '@assets/svg/cart.svg?react'
 
 // redux
@@ -5,15 +7,33 @@ import { useAppSelector } from '@storehooks'
 import { getCartTotalQuantitySelector } from '@storecart/selectors/cartSelectors'
 
 import styles from './styles.module.css'
-const { cartContainer, cartQuantity } = styles
+const { cartContainer, cartQuantity, pumpCartQuantity, cart } = styles
 
 const Cart = () => {
   const cartQty = useAppSelector(getCartTotalQuantitySelector)
 
+  const [isAnimate, setIsAnimate] = useState(false)
+  const quantityStyle = `${cartQuantity} ${isAnimate ? pumpCartQuantity : ''}`
+  useEffect(() => {
+    if (!cartQty) {
+      return
+    }
+    setIsAnimate(true)
+
+    const debounce = setTimeout(() => {
+      setIsAnimate(false)
+    }, 300)
+
+    return () => clearTimeout(debounce)
+  }, [cartQty])
+
   return (
     <div className={cartContainer}>
-      <CartIcon title='cart-icon' />
-      <div className={cartQuantity}> {cartQty} </div>
+      <div className={cart}>
+        <CartIcon title='cart-icon' />
+        <div className={quantityStyle}> {cartQty} </div>
+      </div>
+      <h3>Cart</h3>
     </div>
   )
 }
