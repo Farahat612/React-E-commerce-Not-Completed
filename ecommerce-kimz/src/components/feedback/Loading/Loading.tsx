29bae05ffd637d5d26c0faa227ec63d0fc478901
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-11 11:58:36 +0200

    cretaed Loading component for composition

---
+++ ecommerce-kimz/src/components/feedback/Loading/Loading.tsx
@@ -0,0 +1,21 @@
+// importing TLoading customType
+import { TLoading } from '@customTypesshared'
+
+// defining type of props
+interface LoadingProps {
+  loading: TLoading
+  error: null | string
+  children?: React.ReactNode
+}
+
+const Loading = ({ loading, error, children }: LoadingProps) => {
+  if (loading == 'pending') {
+    return <h1>Loading... Please Wait!</h1>
+  }
+  if (error) {
+    return <h1>failed ... {error}</h1>
+  }
+  return <>{children}</>
+}
+
+export default Loading
