158e3ac47b4e7c851774d99e6e43f665fedeaeda
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-14 12:32:59 +0200

    Code Refactoring and Optimization : Custom hooks
    - created useCart custom hook to handle all Cart page functionalities
    - used it in Cart page

---
+++ ecommerce-kimz/src/hooks/useCart.ts
@@ -0,0 +1,55 @@
+// react hooks
+import { useEffect, useCallback } from 'react'
+// redux
+import { useAppSelector, useAppDispatch } from '@storehooks'
+import {
+  getProductsInfo,
+  itemQtyChange,
+  removeFromCart,
+  cleanCartProductsFullInfo,
+} from '@storecart/cartSlice'
+
+// define the useCart hook
+const useCart = () => {
+  // redux
+  const dispatch = useAppDispatch()
+  const { items, productsFullInfo, loading, error } = useAppSelector(
+    (state) => state.cart
+  )
+  // get the products info
+  useEffect(() => {
+    dispatch(getProductsInfo())
+    return () => {
+      dispatch(cleanCartProductsFullInfo())
+    }
+  }, [dispatch])
+  // map the products info with the items in the cart
+  const products = productsFullInfo.map((el) => ({
+    ...el,
+    quantity: items[el.id], // add the quantity of the item in the cart
+  }))
+  // change the quantity of the item in the cart
+  const changeQuantityHandler = useCallback(
+    (id: number, quantity: number) => {
+      dispatch(itemQtyChange({ id, quantity }))
+    },
+    [dispatch]
+  )
+  // remove the item from the cart
+  const removeItemHandler = useCallback(
+    (id: number) => {
+      dispatch(removeFromCart(id))
+    },
+    [dispatch]
+  )
+
+  return {
+    products,
+    loading,
+    error,
+    changeQuantityHandler,
+    removeItemHandler,
+  }
+}
+
+export default useCart
--- ecommerce-kimz/src/pages/Cart.tsx
+++ ecommerce-kimz/src/pages/Cart.tsx
@@ -1,48 +1,15 @@
-// react imports
-import { useEffect, useCallback } from 'react'
-// redux imports
-import { useAppSelector, useAppDispatch } from '@storehooks'
-import {
-  getProductsInfo,
-  itemQtyChange,
-  removeFromCart,
-  cleanCartProductsFullInfo,
-} from '@storecart/cartSlice'
+// custom hook
+import useCart from '@hooksuseCart'
+
 // Components
 import { Heading } from '@componentsshared'
 import { CartSubTotal, CartItemList } from '@componentseCommerce'
 import { Loading } from '@componentsfeedback'
 
 const Cart = () => {
-  const dispatch = useAppDispatch()
-  const { items, productsFullInfo, loading, error } = useAppSelector(
-    (state) => state.cart
-  )
-  useEffect(() => {
-    dispatch(getProductsInfo())
-    return () => {
-      dispatch(cleanCartProductsFullInfo())
-    }
-  }, [dispatch])
-
-  const products = productsFullInfo.map((el) => ({
-    ...el,
-    quantity: items[el.id],
-  }))
-
-  const changeQuantityHandler = useCallback(
-    (id: number, quantity: number) => {
-      dispatch(itemQtyChange({ id, quantity }))
-    },
-    [dispatch]
-  )
-
-  const removeItemHandler = useCallback(
-    (id: number) => {
-      dispatch(removeFromCart(id))
-    },
-    [dispatch]
-  )
+  // use the custom hook
+  const { products, loading, error, changeQuantityHandler, removeItemHandler } =
+    useCart()
 
   return (
     <>
