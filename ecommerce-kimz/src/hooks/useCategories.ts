79fa3f0e0e9761ce6071a51d0fd8673ca36b538e
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-14 12:03:34 +0200

    Code Refactoring and Optimization : Custom hooks
    - created useCategories custom hook to handle all gategories page functionalities
    - used it in Categories page

---
+++ ecommerce-kimz/src/hooks/useCategories.ts
@@ -0,0 +1,34 @@
+// react hooks
+import { useEffect } from 'react'
+// redux
+import { useAppDispatch, useAppSelector } from '@storehooks'
+import {
+  getCategories,
+  categoriesCleanUp,
+} from '@storecategories/categoriesSlice'
+
+// Defining the useCategories hook
+const useCategories = () => {
+  // redux
+  const dispatch = useAppDispatch()
+  const { records, loading, error } = useAppSelector(
+    (state) => state.categories
+  )
+
+  // Fetching the categories
+  useEffect(() => {
+    const promise = dispatch(getCategories())
+
+    return () => {
+      // Cleaning up the categories slice
+      dispatch(categoriesCleanUp())
+      // Cancelling the request
+      promise.abort()
+    }
+  }, [dispatch])
+
+  return { loading, error, categories: records }
+}
+
+// Exporting the useCategories hook
+export default useCategories
--- ecommerce-kimz/src/pages/Categories.tsx
+++ ecommerce-kimz/src/pages/Categories.tsx
@@ -1,30 +1,15 @@
+// Custom hooks
+import useCategories from '@hooksuseCategories'
 // Components
 import { Category } from '@components/eCommerce'
 import { Loading } from '@componentsfeedback'
 import { GridList, Heading } from '@componentsshared'
-
-// Redux
-import { useAppDispatch, useAppSelector } from '@store/hooks'
-import {
-  getCategories,
-  categoriesCleanUp,
-} from '@store/categories/categoriesSlice'
-import { useEffect } from 'react'
 // Types
 import { ICategory } from '@customTypescategory'
 
 const Categories = () => {
-  const dispatch = useAppDispatch()
-  const { loading, error, records } = useAppSelector(
-    (state) => state.categories
-  )
-
-  useEffect(() => {
-    dispatch(getCategories())
-    return () => {
-      dispatch(categoriesCleanUp())
-    }
-  }, [dispatch])
+  // Getting the categories
+  const { loading, error, categories } = useCategories()
 
   // displaying the categories
   return (
@@ -32,7 +17,7 @@ const Categories = () => {
       <Heading title='Categories' />
       <Loading loading={loading} error={error}>
         <GridList<ICategory>
-          records={records}
+          records={categories}
           renderItem={(record) => <Category {...record} />}
         />
       </Loading>
