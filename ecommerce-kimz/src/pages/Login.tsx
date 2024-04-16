534cc89688507e9c284263e1e3e2cb8a061a2f50
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-17 01:17:39 +0200

    Auth : Login
    - created sign in schema
    - handled login Validation in login page

--- ecommerce-kimz/src/pages/Login.tsx
+++ ecommerce-kimz/src/pages/Login.tsx
@@ -1,42 +1,56 @@
+// react-hook-form
+import { useForm, SubmitHandler } from 'react-hook-form'
+// zod
+import { zodResolver } from '@hookform/resolvers/zod'
+import { signInSchema, signInType } from '@validations/signInSchema'
 // bootstrap
 import { Form, Button, Row, Col } from 'react-bootstrap'
 // components
 import { Heading } from '@components/shared'
-
-
-// Form inputs type
-// type TFormInputs = {
-//   email: string
-//   password: string
-// }
+import { Input } from '@components/form'
 
 const Login = () => {
+  // Destructuring useForm into register
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<signInType>(
+    // zodResolver to validate form inputs
+    {
+      mode: 'onBlur',
+      resolver: zodResolver(signInSchema),
+    }
+  )
+
+  // submitForm function
+  const submitForm: SubmitHandler<signInType> = (data) => {
+    console.log(data)
+  }
+
   return (
     <>
       <Heading title='User Login' />
-      <Row>
+      <Row className='my-5'>
         <Col md={{ span: 6, offset: 3 }}>
-          <Form>
-            <Form.Group className='mb-3'>
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type='email'
-                placeholder='Enter email'
-                name='email'
-              />
-            </Form.Group>
+          <Form onSubmit={handleSubmit(submitForm)}>
+            <Input
+              label='Email'
+              name='email'
+              register={register}
+              error={errors.email?.message}
+            />
 
-            <Form.Group className='mb-3'>
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type='password'
-                placeholder='Password'
-                name='password'
-              />
-            </Form.Group>
+            <Input
+              label='Password'
+              name='password'
+              type='password'
+              register={register}
+              error={errors.password?.message}
+            />
 
             <Button variant='info' type='submit' style={{ color: 'white' }}>
-              Register
+              Login
             </Button>
           </Form>
         </Col>
