33620e812e3661bdf73de7900810090b230f4efe
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-16 14:31:22 +0200

    Auth : react-hook-form
    - installed react-hook-form
    - Register Page initial
    - Login page initial

--- ecommerce-kimz/src/pages/Login.tsx
+++ ecommerce-kimz/src/pages/Login.tsx
@@ -1,8 +1,43 @@
+// bootstrap
+import { Form, Button, Row, Col } from 'react-bootstrap'
+// components
+import { Heading } from '@components/shared'
+
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <>
+      <Heading title='User Login' />
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form>
+            <Form.Group className='mb-3'>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Enter email'
+                name='email'
+              />
+              <Form.Text className='text-muted'>
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className='mb-3'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Password'
+                name='password'
+              />
+            </Form.Group>
+
+            <Button variant='info' type='submit' style={{ color: 'white' }}>
+              Register
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </>
   )
 }
 
--- ecommerce-kimz/src/pages/Register.tsx
+++ ecommerce-kimz/src/pages/Register.tsx
@@ -1,8 +1,70 @@
+// bootstrap
+import { Form, Button, Row, Col } from 'react-bootstrap'
+// components
+import { Heading } from '@components/shared'
+
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+    <>
+      <Heading title='User Registration' />
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form>
+            <Form.Group className='mb-3'>
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter first name'
+                name='firstName'
+              />
+            </Form.Group>
+
+            <Form.Group className='mb-3'>
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter last name'
+                name='lastName'
+              />
+            </Form.Group>
+
+            <Form.Group className='mb-3'>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Enter email'
+                name='email'
+              />
+              <Form.Text className='text-muted'>
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className='mb-3'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Password'
+                name='password'
+              />
+            </Form.Group>
+
+            <Form.Group className='mb-3'>
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Confirm Password'
+                name='confirmPassword'
+              />
+            </Form.Group>
+
+            <Button variant='info' type='submit' style={{ color: 'white' }}>
+              Register
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </>
   )
 }
 
