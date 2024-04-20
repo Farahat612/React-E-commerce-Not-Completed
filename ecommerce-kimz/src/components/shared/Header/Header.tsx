5bd0206c73ab0c58852ebdab6e2656453f207e40
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-20 13:48:01 +0200

    Auth : Header
    - manipulated the Header to change navbar right part according to auth state
    - added logout functionality

--- ecommerce-kimz/src/components/shared/Header/Header.tsx
+++ ecommerce-kimz/src/components/shared/Header/Header.tsx
@@ -1,15 +1,32 @@
-// third party imports
+// react imports
+import { useEffect } from 'react'
+// react-router-dom imports
 import { NavLink } from 'react-router-dom'
-import { Badge, Navbar, Nav, Container } from 'react-bootstrap'
-
+// redux imports
+import { useAppDispatch, useAppSelector } from '@store/hooks'
+import { authLogout } from '@store/auth/authSlice'
+import { getWishlist } from '@store/wishlist/wishlistSlice'
+// bootstrap imports
+import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 // components
 import HeaderRightBar from './HeaderRightBar/HeaderRightBar'
-
 // styles
 import styles from './styles.module.css'
 const { headerContainer, headerLogo } = styles
 
 const Header = () => {
+  // initializing the dispatch
+  const dispatch = useAppDispatch()
+  // getting the auth state
+  const { accessToken, user } = useAppSelector((state) => state.auth)
+
+  // getting the wishlist
+  useEffect(() => {
+    if (accessToken) {
+      dispatch(getWishlist())
+    }
+  }, [dispatch, accessToken])
+
   return (
     <header>
       <div className={headerContainer}>
@@ -43,12 +60,34 @@ const Header = () => {
               </Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link as={NavLink} to='login'>
-                Login
-              </Nav.Link>
-              <Nav.Link as={NavLink} to='register'>
-                Register
-              </Nav.Link>
+              {!accessToken ? (
+                <>
+                  <Nav.Link as={NavLink} to='login'>
+                    Login
+                  </Nav.Link>
+                  <Nav.Link as={NavLink} to='register'>
+                    Register
+                  </Nav.Link>
+                </>
+              ) : (
+                <NavDropdown
+                  title={`Welcome: ${user?.firstName} ${user?.lastName}`}
+                  id='basic-nav-dropdown'
+                >
+                  <NavDropdown.Item as={NavLink} to='profile'>
+                    Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>Orders</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item
+                    as={NavLink}
+                    to='/'
+                    onClick={() => dispatch(authLogout())}
+                  >
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
